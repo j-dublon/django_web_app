00722c52ccef3f0b2f68ce5ed5f0a8846a3fe8d0
user1 <user1@gmail.com>
2025-03-25 15:22:21 +0000

    Add basic template

--- first_app/models.py
+++ first_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Product(models.Model):
+    def __str__(self):
+        return self.name
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     desc = models.CharField(max_length=200)
--- first_app/views.py
+++ first_app/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 
 def index(request):
     return HttpResponse("Hello World")
 
 def products(request):
-    products = ["Iphone", "Mac", "IPad"]
-    return HttpResponse(products)
+    products = Product.objects.all()
+    context = {
+        "products": products
+    }
+    return render(request, 'first_app/index.html', context)
